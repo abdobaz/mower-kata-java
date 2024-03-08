ac636a43a188fa73934e861b982632377730f6f3
Abderrahim <abderrahim.m.elbaz@gmail.com>
2024-03-08 11:45:43 +0100

    [Mower-printer] test passed (kiss)

--- src/main/java/com/socgen/mowit/infrastructure/adapter/MowerPrinterAdapter.java
+++ src/main/java/com/socgen/mowit/infrastructure/adapter/MowerPrinterAdapter.java
@@ -3,9 +3,16 @@ package com.socgen.mowit.infrastructure.adapter;
 import com.socgen.mowit.domain.Mower;
 import com.socgen.mowit.domain.port.MowerPrinterPort;
 
+import java.util.Objects;
+import java.util.logging.Logger;
+
+
 public class MowerPrinterAdapter implements MowerPrinterPort {
     @Override
     public void printMower(Mower mower) {
-        throw new  UnsupportedOperationException("Not yest Implemented");
+        var log = Logger.getLogger(MowerPrinterAdapter.class.getName());
+        if(Objects.nonNull(mower)) {
+            log.info("mower.toString()");
+        }
     }
 }
--- src/test/java/com/socgen/mowit/infrastructure/adapter/MowerPrinterAdapterTest.java
+++ src/test/java/com/socgen/mowit/infrastructure/adapter/MowerPrinterAdapterTest.java
@@ -3,25 +3,31 @@ package com.socgen.mowit.infrastructure.adapter;
 import com.socgen.mowit.domain.Mower;
 import com.socgen.mowit.domain.MowerPosition;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+import java.util.logging.Logger;
 
 import static com.socgen.mowit.domain.EnumDirection.EAST;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.only;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class MowerPrinterAdapterTest {
 
     private final MowerPrinterAdapter mowerPrinterAdapter = new MowerPrinterAdapter();
+
     @Test
     void should_print_mower_information() {
         //GIVEN
         var mowerInitialPosition = new MowerPosition(5, 5);
         var mower = new Mower(mowerInitialPosition, EAST);
+        try(MockedStatic<Logger> loggerFactoryMockedStatic = mockStatic(Logger.class)) {
+            var logger = mock(Logger.class);
+            loggerFactoryMockedStatic.when(()-> Logger.getLogger(MowerPrinterAdapter.class.getName()))
+                    .thenReturn(logger);
+            //WHEN
+            mowerPrinterAdapter.printMower(mower);
 
-        //WHEN
-        mowerPrinterAdapter.printMower(mower);
-
-        //THEN check logger was invoked
-       fail("check logger was invoked not yet implemented");
+            //THEN check logger was invoked
+            verify(logger).info("mower.toString()");
+        }
     }
 }
